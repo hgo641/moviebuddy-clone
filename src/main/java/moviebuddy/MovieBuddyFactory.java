b874680d8b7d3b49a9e102cf5ee52f76a33eb7f4
hgo641 <hgo641@gmail.com>
2022-08-06 09:40:29 +0900

    modify: MovieBuddyFactory

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -1,18 +1,33 @@
 package moviebuddy;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
 
 @Configuration
 public class MovieBuddyFactory {
+
+	@Configuration
+	static class DomainModuleConfig{
+		@Bean
+		public MovieFinder movieFinder(MovieReader movieReader) {
+			return new MovieFinder(movieReader);
+		}
+		
+	}
 	
-	@Bean
-	public MovieFinder movieFinder() {
-		return new MovieFinder(new CsvMovieReader());
+	@Configuration
+	static class DataSourceModuleConfig {
+		@Bean
+		public MovieReader movieReader() {
+			return new CsvMovieReader();
+		}
 	}
 
 }
